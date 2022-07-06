bbd94071f57f2f435e6d78a4cea9a78ff09c1bcf
Shauna Han <90071614+shaunahan@users.noreply.github.com>
2022-07-05 23:26:23 -0400

    Update insights.py

--- ETP_pipelines_in_python/insights.py
+++ ETP_pipelines_in_python/insights.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul  5 22:35:36 2022
 
-@author: user
+@author: shaunahan
 """
 
+from common.base import session
+
+# Create the view with the appropriate metrics
+query = """
+CREATE OR REPLACE VIEW insights AS
+SELECT county,
+       COUNT(*) AS sales_count,
+       SUM(CAST(price AS int)) AS sales_total,
+       MAX(CAST(price AS int)) AS sales_max,
+       MIN(CAST(price AS int)) AS sales_min,
+       AVG(CAST(price AS int))::numeric(10,2) AS sales_avg
+FROM ppr_clean_all
+GROUP BY county
+"""
+
+# Execute and commit
+session.execute(query)
+session.commit()
